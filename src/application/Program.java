c352f6f7a7a1a17c3af11bb5150da8721439d2d6
Diogo Araujo <diogo.arauj@gmail.com>
2020-08-10 16:10:18 -0300

    Projeto atualizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,23 @@
 package application;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Buenos dyas!");
-		System.out.println("Boa tarde!!");
-		System.out.println("Boa noite!!!");
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 	}
 }
